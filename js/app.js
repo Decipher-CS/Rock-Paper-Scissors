d2278979d4a87a2ea7129c02ae1f8da7cd16710b
de-sipher <0mu5sfm4@anonaddy.me>
2022-04-06 22:49:25 +0530

    progress made, section 1 designed.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,151 @@
-//use a map for this.
-const rockToken = "rockToken";
-const paperToken = "paperToken";
-const scissorsToken = "scissorsToken";
+var token = {
+    rockToken: {
+        "background": "linear-gradient(90deg, hsl(349, 71%, 52%), hsl(349, 70%, 56%));",
+        "url": "/images/icon-rock.svg",
+        "strength": 1
+    },
+    paperToken: {
+        "background": "linear-gradient(90deg, hsl(230, 89%, 62%), hsl(230, 89%, 65%))",
+        "url": "/images/icon-paper.svg",
+        "strength": 3
+    },
+    scissorsToken: {
+        "background": "linear-gradient(90deg,hsl(39, 89%, 49%),hsl(40, 84%, 53%)); ",
+        "url": "/images/icon-scissors.svg",
+        "strength": 5
+    }
+}
+
+
+
+// Const/ var declared here :
+
+const scoreTag = document.querySelector(".heading__score__value");
+const localStorage = window.localStorage
+localStorage.setItem("score", "0")
+
+const rockToken = token.rockToken;
+const paperToken = token.paperToken;
+const scissorsToken = token.scissorsToken;
+
+const placeholderUser = document.querySelector(".token-placeholder__user-choice");
+const placeholderComp = document.querySelector(".token-placeholder__comp-choice");
+
+const tokenSelection = document.querySelector(".choice-section");
+const compSelection = document.querySelector(".token-placeholder");
+
+const retry = document.querySelector(".result-declaration__play-again");
+
+let playerToken = undefined;
+let compToken = undefined;
+let roundWinner = undefined
+
+// Functions here :
+
+function createToken(tokenType) {
+    var node = document.createElement("img");
+    node.setAttribute("src", tokenType.url)
+    return node
+}
+
+function decorateToken(node, tokenType) {
+    let token = createToken(tokenType);
+    if (node.firstChild) {
+        node.removeChild(node.firstChild)
+    }
+
+    node.appendChild(token)
+
+    node.style.cssText = `
+    display: flex;
+    overflow: hidden;
+  
+    justify-content: center;
+    align-items: center;
+  
+    aspect-ratio: 1/1;
+    width: 70%;
+  
+    background: ${tokenType.background};
+    border-radius: 50%;
+    box-shadow: inset rgba(20, 20, 20, 0.3) 0px -7px 0px -1px;
+  
+    cursor: pointer;
+                        `
+    // node.addClass("winner-animation")
+
+    node.firstChild.style.cssText = `
+    display: inline-block;
+ 
+    aspect-ratio: 1/1;
+    width: 70%;
+    object-fit: contain;
+  
+    padding: 16%;
+  
+    background-color: white;
+    border-radius: 50%;
+    box-shadow: inset hsl(0, 2%, 32%, 15%) 0px 1em 0px -10px;
+                        `
+}
+
+let compChoice = () => {
+    let tokens = [rockToken, paperToken, scissorsToken]
+    let num = Math.floor(Math.random() * 3)
+    return (tokens[num])
+}
+
+let winner = (playerToken, compToken) => {
+    if (playerToken == compToken) {
+        return 0
+    }
+
+    var sum = playerToken.strength + compToken.strength
+
+    if (sum == 4) {
+        return paperToken
+    } else if (sum == 6) {
+        return rockToken
+    } else if (sum == 8) {
+        return scissorsToken
+    }
+}
+
+
+// Event listners
+
+if (tokenSelection) {
+
+    tokenSelection.addEventListener("click", (token) => {
+        var tokenType = String(token.target.classList)
+
+        tokenSelection.style.display = "none"
+        compSelection.style.display = "grid"
+
+        if (tokenType.includes("rock")) {
+            decorateToken(placeholderUser, rockToken)
+            playerToken = rockToken
+        } else if (tokenType.includes("paper")) {
+            decorateToken(placeholderUser, paperToken)
+            playerToken = paperToken
+        } else if (tokenType.includes("scissors")) {
+            decorateToken(placeholderUser, scissorsToken)
+            playerToken = scissorsToken
+        }
+    })
+}
+
+
+const observer = new IntersectionObserver((entries, observer) => {
+    if (entries[0].isIntersecting) {
+        compToken = compChoice()
+        decorateToken(placeholderComp, compToken)
+        roundWinner = winner(playerToken, compToken)
+    }
+})
+observer.observe(compSelection)
+
+retry.onclick = function () {
+    tokenSelection.style.display = "grid"
+    compSelection.style.display = "none"
+}
